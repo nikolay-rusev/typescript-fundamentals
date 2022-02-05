8ea168e06cb7b67e026100171a4caf643695040e
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2022-02-06 01:24:56 +0200

    class methods

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import { generateRandomId } from './utils';
 
+type Element = HTMLElement | null;
+
 class App {
     static id = 'app';
+    onInit(el: Element): void {
+        setInterval(() => {
+            if (el) {
+                el.innerHTML = generateRandomId({symbol: '#', length: 7});
+            }
+        }, 1000);
+    }
 }
 
 function main(ComponentClass: any) {
-    const cmp = ComponentClass();
-    let app = document.getElementById(ComponentClass.id);
-    setInterval(() => {
-        if (app) {
-            app.innerHTML = generateRandomId({symbol: '#', length: 7});
-        }
-    }, 1000);
+    const el = document.getElementById(ComponentClass.id);
+    const cmp = new ComponentClass();
+    cmp.onInit(el);
 }
 
 main(App);
